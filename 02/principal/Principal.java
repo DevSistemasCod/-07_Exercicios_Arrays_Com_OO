c2b1d97a4f22d2833b433e2cab4bc9c91efef154
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-04-03 00:04:30 -0300

    Add files via upload

---
+++ 02/principal/Principal.java
@@ -0,0 +1,45 @@
+package principal;
+
+import java.util.Scanner;
+
+import entidades.Venda;
+
+public class Principal {
+	    public static void main(String[] args) {
+	        //int[] parcelas = { 2, 4, 6 };
+	    	//int[] valorParcelas = venda.calcularParcelas(3);
+	        int[] valorParcelas;
+	        Scanner lerDados = new Scanner(System.in);
+	        Venda venda = new Venda();
+	        
+	        //Venda venda = new Venda(1, "Gol", 40000.00, "João", parcelas);
+	        
+	        System.out.println("Informe o nome do vendedor: ");
+	        venda.setNomeVendedor(lerDados.next());
+	        
+	        System.out.println("Informe o modelo do carro: ");
+	        venda.setModeloCarro(lerDados.next());
+	        
+	        System.out.println("Informe o valor da venda: ");
+	        venda.setValorVenda(lerDados.nextFloat());
+	        
+	        System.out.println("Informe o número da venda: ");
+	        venda.setNumVenda(lerDados.nextInt());
+	        
+	        System.out.println("Informe o número de parcelas: ");
+	        valorParcelas = venda.calcularParcelas(lerDados.nextInt());
+	        
+	        System.out.println("Número da venda: " + venda.getNumVenda());
+	        System.out.println("Modelo do carro: " + venda.getModeloCarro());
+	        System.out.println("Valor da venda: " + venda.getValorVenda());
+	        System.out.println("Nome do vendedor: " + venda.getNomeVendedor());
+
+	        for (int i = 0; i < valorParcelas.length; i++) {
+	            System.out.printf("\nValor da parcela[%d]: %d",i+1, valorParcelas[i]);
+	        }
+	        
+	        lerDados.close();
+	    }
+	
+
+}
